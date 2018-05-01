94fa191a066415e774c56afbd8cd682c7cee5358
alpo25 <alpo25@gmail.com>
2018-05-01 12:52:51 -0300

    algo

--- server.js
+++ server.js
@@ -73,18 +73,8 @@ app.get('/', function (req, res) {
     initDb(function(err){});
   }
   if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+    res.send('{ pageCount: -1 }');
+  } 
 });
 
 app.get('/pagecount', function (req, res) {
