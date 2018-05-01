e106b5a254c3fa1eb52998379ba00df679ec8ab8
alpo25 <alpo25@gmail.com>
2018-05-01 13:03:23 -0300

    algo

--- server.js
+++ server.js
@@ -69,12 +69,8 @@ var initDb = function(callback) {
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
     res.send('{ pageCount: -1 }');
-  } 
+
 });
 
 app.get('/pagecount', function (req, res) {
