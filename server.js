b76c0f57aa0b0b1ceda4cd2ac089452ea52155b2
alpo25 <alpo25@gmail.com>
2018-05-01 12:06:10 -0300

    algo

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //  OpenShift sample Node application
 var express    = require('express'),
     app        = express(),
-   // Task       = require('./api/models/todoListModel'),     
     bodyParser = require('body-parser'),    
     morgan     = require('morgan');
 
@@ -10,20 +9,21 @@ Object.assign=require('object-assign')
 //app.engine('html', require('ejs').renderFile);
 //app.use(morgan('combined'))
 //app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//
-//var router = express.Router();
+app.use(methodOverride());
 
-app.get('/po', function(req, res) {
-   res.end("{Hello:World}");
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
 });
 
-//routes(app); //register the route
-/*
+routes(app); //register the route
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
@@ -141,4 +141,3 @@ console.log('Server running on http://%s:%s', ip, port);
 
 
 module.exports = app ;
-*/
