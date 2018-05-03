fc40f166d3165d7bf2b88c2070cb6d32b1742a14
alpo25 <alpo25@gmail.com>
2018-05-02 22:29:27 -0300

    algo

--- server.js
+++ server.js
@@ -63,6 +63,59 @@ var initDb = function(callback) {
   });
 };
 
+/**
+* Generates number of random geolocation points given a center and a radius.
+* @param  {Object} center A JS object with lat and lng attributes.
+* @param  {number} radius Radius in meters.
+* @param {number} count Number of points to generate.
+* @return {array} Array of Objects with lat and lng attributes.
+*/
+function generateRandomPoints(center, radius, count) {
+  var points = [];
+  for (var i=0; i<count; i++) {
+    points.push(generateRandomPoint(center, radius));
+  }
+  return points;
+}
+
+
+/**
+* Generates number of random geolocation points given a center and a radius.
+* Reference URL: http://goo.gl/KWcPE.
+* @param  {Object} center A JS object with lat and lng attributes.
+* @param  {number} radius Radius in meters.
+* @return {Object} The generated random points as JS object with lat and lng attributes.
+*/
+function generateRandomPoint(center, radius) {
+  var x0 = center.lng;
+  var y0 = center.lat;
+  // Convert Radius from meters to degrees.
+  var rd = radius/111300;
+
+  var u = Math.random();
+  var v = Math.random();
+
+  var w = rd * Math.sqrt(u);
+  var t = 2 * Math.PI * v;
+  var x = w * Math.cos(t);
+  var y = w * Math.sin(t);
+
+  var xp = x/Math.cos(y0);
+
+  // Resulting point.
+  return {'lat': y+y0, 'lng': xp+x0};
+}
+
+app.post('/randomPoints', function (req, res) {
+  console.log('reqbody: '+JSON.stringify(req.body));
+  // Usage Example.
+  // Generates 100 points that is in a 1km radius from the given lat and lng point.
+  var randomGeoPoints = generateRandomPoints({'lat':req.body.latitude, 'lng':req.body.longitude}, 500, 10);
+  res.contentType('application/json');
+  res.send(JSON.stringify(randomGeoPoints));
+
+});
+
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
