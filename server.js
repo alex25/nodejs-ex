6118336921b6239c0e953064c7478465a8e81a09
alpo25 <alpo25@gmail.com>
2018-05-09 22:31:21 -0300

    algo

--- server.js
+++ server.js
@@ -186,11 +186,11 @@ app.post('/updateUserData', function (req, res) {
     }
     if (db) {
       var collection = db.collection('users');
-      collection.update({"_id":ObjectId(req.body._id)},{ $set: {"latitude":req.body.latitude, longitude:req.body.longitude}},
+      collection.update({"_id":ObjectId(req.body.userId)},{ $set: {"latitude":req.body.latitude, "longitude":req.body.longitude}},
           function( err, resp) {
             if ( err ) throw err;
             //console.log(resp[0]);
-            collection.find({"_id":{$ne:ObjectId(req.body._id)}}).toArray(function(err, docs) { //,{latitude:{$ne:"0"}}) {userName:{$ne:req.body.userName}},{latitude:{$ne:"0"}}
+            collection.find({"_id":{$ne:ObjectId(req.body.userId)}}).toArray(function(err, docs) { //,{latitude:{$ne:"0"}}) {userName:{$ne:req.body.userName}},{latitude:{$ne:"0"}}
               //imprimimos en la consola el resultado
               
               for(i in docs){
